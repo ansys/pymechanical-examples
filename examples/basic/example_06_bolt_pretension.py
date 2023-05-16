9d74f734c755db185c0ac74573fb7d1f7123f570
Saminathan Sangili <sam.sangili@ansys.com>
2023-05-15 23:02:22 -0400

    Adjust the tests - so tests run end to end

--- examples/basic/example_06_bolt_pretension.py
+++ examples/basic/example_06_bolt_pretension.py
@@ -25,7 +25,7 @@ from ansys.mechanical.core.examples import download_file
 # ``False``. To close this Mechanical session when finished, this example
 # must call  the ``mechanical.exit()`` method.
 
-mechanical = launch_mechanical(batch=False, cleanup_on_exit=False)
+mechanical = launch_mechanical(batch=True, cleanup_on_exit=False)
 print(mechanical)
 
 ###############################################################################
