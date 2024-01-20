9ed4c6d441686a166d0c79b02195408a986c0fb3
dkunhamb <dipin.kunhambunair@ansys.com>
2024-01-19 18:20:57 -0600

    add image and gif export

--- examples/basic/example_08_lsdyna_taylor_bar.py
+++ examples/basic/example_08_lsdyna_taylor_bar.py
@@ -15,8 +15,12 @@ and plastic strain (EPS) animation is exported in the project directory.
 # Download the required files. Print the file path for the geometry file.
 import os
 
+from PIL import Image
 from ansys.mechanical.core import launch_mechanical
 from ansys.mechanical.core.examples import download_file
+from matplotlib import image as mpimg
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 geometry_path = download_file("example_08_Taylor_Bar.agdb", "pymechanical", "00_basic")
 print(f"Downloaded the geometry file to: {geometry_path}")
@@ -138,7 +142,8 @@ eps.Expression = "EPS"
 eps.EvaluateAllResults()
 eps_max = eps.Maximum
 eps_min = eps.Minimum
-
+total_deformation = analysis.Solution.AddTotalDeformation()
+total_deformation.EvaluateAllResults()
 # Set Camera
 Graphics.Camera.FocalPoint = Point([9.0521184381880495,
                                     2.9680547361873595,
@@ -160,11 +165,24 @@ Graphics.ViewOptions.ResultPreference.DeformationScaling = true_scale
 
 Graphics.ViewOptions.ResultPreference.DeformationScaleMultiplier = 1
 
+
 # Export an animation
-anim_file_path = os.path.join(project_directory, "taylor_bar.avi")
-#eps.ExportAnimation(anim_file_path,
-#                    GraphicsAnimationExportFormat.AVI,
-#                    Ansys.Mechanical.Graphics.AnimationExportSettings(2000.0, 1000.0))
+eps.Activate()
+animation_export_format = GraphicsAnimationExportFormat.GIF
+settings_720p = Ansys.Mechanical.Graphics.AnimationExportSettings()
+settings_720p.Width = 1280
+settings_720p.Height = 720
+anim_file_path = os.path.join(project_directory, "taylor_bar.gif")
+eps.ExportAnimation(
+    anim_file_path, animation_export_format, settings_720p
+)
+
+# Set the isometric view and zoom to fit.
+Graphics.Camera.Pan(Quantity(7, 'mm'),Quantity(4, 'mm'))
+total_deformation.Activate()
+image_path = os.path.join(project_directory, "totaldeformation.png")
+Graphics.ExportImage(image_path, GraphicsImageExportFormat.PNG)
+
 
 dir_deformation_details = {
 "Minimum": str(eps_max),
@@ -175,6 +193,14 @@ json.dumps(dir_deformation_details)
 """
 output = mechanical.run_python_script(mech_act_code)
 print(output)
+
+# image_directory_modified = project_directory.replace("\\", "\\\\")
+mechanical.run_python_script(f"image_dir=ExtAPI.DataModel.Project.ProjectDirectory")
+
+
+# Verify the path for image directory.
+result_image_dir_server = mechanical.run_python_script(f"image_dir")
+print(f"Images are stored on the server at: {result_image_dir_server}")
 
 ###############################################################################
 # Download output file from solve and print contents
@@ -205,6 +231,7 @@ print("Getting solve out path")
 solve_out_path = get_solve_out_path(mechanical)
 print(f"Solve out path (on remote machine): {solve_out_path}")
 
+
 if solve_out_path != "":
     current_working_directory = os.getcwd()
 
@@ -216,6 +243,69 @@ if solve_out_path != "":
     write_file_contents_to_console(solve_out_local_path)
     print("Printed output to console")
     os.remove(solve_out_local_path)
+
+###############################################################################
+# Get image and display
+# ~~~~~~~~~~~~~~~~~~~~~~
+
+
+def get_image_path(image_name):
+    return os.path.join(result_image_dir_server, image_name)
+
+
+def display_image(path):
+    print(f"Printing {path} using matplotlib")
+    image1 = mpimg.imread(path)
+    plt.figure(figsize=(15, 15))
+    plt.axis("off")
+    plt.imshow(image1)
+    plt.show()
+
+
+image_name = "totaldeformation.png"
+image_path_server = get_image_path(image_name)
+
+if image_path_server != "":
+    current_working_directory = os.getcwd()
+
+    local_file_path_list = mechanical.download(
+        image_path_server, target_dir=current_working_directory
+    )
+    image_local_path = local_file_path_list[0]
+    print(f"Local image path : {image_local_path}")
+
+    display_image(image_local_path)
+
+###############################################################################
+# Get gif and display
+# ~~~~~~~~~~~~~~~~~~~
+
+animation_name = "taylor_bar.GIF"
+animation_server = get_image_path(animation_name)
+
+
+def update(frame):
+    gif.seek(frame)
+    img.set_array(gif.convert("RGBA"))
+    return [img]
+
+
+if image_path_server != "":
+    current_working_directory = os.getcwd()
+
+    local_file_path_list = mechanical.download(
+        animation_server, target_dir=current_working_directory
+    )
+    image_local_path = local_file_path_list[0]
+    print(f"Local image path : {image_local_path}")
+    gif = Image.open(image_local_path)
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.axis("off")
+    img = ax.imshow(gif.convert("RGBA"))
+    ani = FuncAnimation(
+        fig, update, frames=range(gif.n_frames), interval=100, repeat=True, blit=True
+    )
+    plt.show()
 
 ###########################################################
 # Close Mechanical
