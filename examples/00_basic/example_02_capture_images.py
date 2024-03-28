fc321ecbf06d15f7124e7476464d051412f99723
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2024-03-28 13:52:11 +0100

    Fix Mechanical name as it is a product. (#197)

--- examples/00_basic/example_02_capture_images.py
+++ examples/00_basic/example_02_capture_images.py
@@ -32,7 +32,7 @@ script_file_path = download_file(
 print(f"Downloaded the script files to: {script_file_path}")
 
 # %%
-# Launch mechanical
+# Launch Mechanical
 # ~~~~~~~~~~~~~~~~~
 # Launch a new Mechanical session in batch, setting the ``cleanup_on_exit``
 # argument to ``False``. To close this Mechanical session when finished,
@@ -136,8 +136,8 @@ if image_path_server != "":
 mechanical.clear()
 
 ###########################################################
-# Close mechanical
+# Close Mechanical
 # ~~~~~~~~~~~~~~~~
-# Close the mechanical instance.
+# Close the Mechanical instance.
 
 mechanical.exit()
